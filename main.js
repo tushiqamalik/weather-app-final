0daea0022b8dac2f740215dcb3cb971cee00634b
tushiqamalik <105580656+tushiqamalik@users.noreply.github.com>
2023-12-05 18:00:58 +0530

    commit

---
+++ main.js
@@ -0,0 +1,22 @@
+console.group("Main.js working");
+
+const apiKey = "59e074ea9d678af5b31903a63ab1cf92";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " kmph";
+}
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
